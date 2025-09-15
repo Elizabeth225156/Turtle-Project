e5a6a5280b070723161eaece2ae6a03497fad4e4
Elizabeth225156 <elizabeth.elden@mcvts.org>
2025-09-15 10:39:31 -0400

    Add files via upload

---
+++ turtleProject.py
@@ -0,0 +1,22 @@
+import turtle
+import random
+
+t = turtle.Turtle()
+
+t.setpos(10, 10)
+t.circle(50)
+
+# t.hideturtle()
+# t.pensize(3)
+
+# colors = 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black'
+
+# for i in range (7):
+#     t.color(colors[i - 1])
+#     t.begin_fill()
+#     for i in range (4):
+#         t.forward(50)
+#         t.left(90)
+#     t.end_fill()
+
+turtle.done()
