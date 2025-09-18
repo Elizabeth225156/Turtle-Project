3159a29c983c486ec7d50c3f8d5646e246774b1a
Elizabeth225156 <elizabeth.elden@mcvts.org>
2025-09-18 10:42:04 -0400

    Add files via upload

--- turtleProject.py
+++ turtleProject.py
@@ -8,11 +8,17 @@ screen.setup(width=850, height=650)
 #Set up the initial turtle
 t.color('white')
 t.hideturtle()
+
+# CHOOSE WHICH ONE
 t.speed(0)
+# screen.tracer(0)
 
 #Function to draw stars
 def drawStar(xCord, yCord):
-    t.teleport(xCord, yCord)
+    t.penup()
+    t.goto(xCord, yCord)
+    t.pendown()
+    # t.teleport(xCord, yCord)
     t.dot(3, 'white')
 
 #background as space
@@ -28,18 +34,31 @@ for i in range (numOfStars):
 for i in range (numOfStars):
     drawStar(xCordStar[i], yCordStar[i])
 
-#Planet outline
+#Planet outline + blue color for water
 t.penup()
 t.goto(0, -300)
 t.pendown()
+t.color('#58b1f5')
+t.begin_fill()
 t.circle(300)
+t.end_fill()
+t.color('#1d5212')
+t.showturtle()
+
+#Basic continent
+t.left(90)
+for i in range (100):
+    t.forward(random.randint(0, 5))
+    t.left(random.randint(0, 5))
+    t.forward(random.randint(0, 5))
+    t.right(random.randint(0, 5))
+
 
 #Thingy for drawing the planet
 #Draw outlines of continents
     #Find two points on the outline
     #Randomly generate a squiggly line between them
 #Color in continents
-#Color in ocean. (trace oceans first?)
 
 turtle.done()
 
